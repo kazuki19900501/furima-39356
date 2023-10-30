334c837c5b4b0d2f8d3cb29042edfb6856f19532
kazuki19900501 <fkazuki19900501@gmail.com>
2023-10-30 21:26:26 +0900

    詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,14 @@ class ItemsController < ApplicationController
     @items = Item.order(created_at: :desc)
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :description, :cotegory_id, :condition_id, :delivery_charge_id, :prefecture_id, :transit_day_id, :price).merge(user_id: current_user.id)
