216b8ee30b8aca66ff7821d547a1ff5c8371fafd
kazuki19900501 <fkazuki19900501@gmail.com>
2023-11-01 14:22:38 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,10 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user == current_user
+      redirect_to root_path
+      return
+    end
   end
 
   def show
